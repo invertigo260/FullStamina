b82f3398537dd2f706442e9c1c90590fdac3e15b
invertigo260 <vert260@gmail.com>
2023-12-08 00:34:57 -0700

    basic start of full stamina script

---
+++ FullStam/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,22 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FullStam.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+
+        [HarmonyPatch("LateUpdate")]
+        [HarmonyPostfix]
+        static void FullSprintPatch(ref float ___sprintMeter)
+        {
+        ___sprintMeter = 1f;
+        }
+
+
+    }
+}
---
+++ FullStam/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullStam
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string ModGUID = "Invertigo.FullStam";
+        private const string ModName = "FullStam";
+        private const string ModVersion = "1.0.0.0";
+
+        private readonly HarmonyLib.Harmony harmony = new HarmonyLib.Harmony(ModGUID);
+        private static Plugin Instance;
+
+
+        private void Awake()
+        {
+            BepInEx.Logging.ManualLogSource
+                        // Plugin startup logic
+
+                        mylogging = BepInEx.Logging.Logger.CreateLogSource(ModGUID);
+            mylogging.LogInfo($"Plugin {ModGUID} is loaded!");
+
+            if ( Instance == null )
+            {
+                Instance = this;
+            }
+
+            harmony.PatchAll(typeof(Plugin));
+
+        }
+    }
+}
